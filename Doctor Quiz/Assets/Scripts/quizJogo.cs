0381a3cc74c5151f74365563f514ac2ff5d3a5dc
João Lucas <johndescraft@gmail.com>
2023-05-28 09:30:35 -0300

    First Commit
    Commit teste

---
+++ Doctor Quiz/Assets/Scripts/quizJogo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quizJogo : MonoBehaviour
+{
+
+    public Button   btnContinuar;
+
+    private int     idTema;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnContinuar.interactable = false;
+    }
+
+    
+    public void selecioneTema()
+    {
+        btnContinuar.interactable = true;
+    }
+}
---
+++ Doctor Quiz/Assets/Scripts/responder.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class responder : MonoBehaviour
+{
+
+    public Text pergunta;
+    public Text txtQuestionA;
+    public Text txtQuestionB;
+    public Text txtQuestionC;
+    public Text txtQuestionD;
+
+    public string[] perguntas;
+    public string[] alternativaA;
+    public string[] alternativaB;
+    public string[] alternativaC;
+    public string[] alternativaD;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
