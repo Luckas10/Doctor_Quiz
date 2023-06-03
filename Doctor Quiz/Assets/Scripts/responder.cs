a736807ee6cf82f3e17df10ae272fa5a4605afd0
Junior <69211250+juniorjse@users.noreply.github.com>
2023-06-03 18:51:15 -0300

    update

--- Doctor Quiz/Assets/Scripts/responder.cs
+++ Doctor Quiz/Assets/Scripts/responder.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-public class responder : MonoBehaviour
+public class Responder : MonoBehaviour
 {
     public Text questionText;
+    public Image questionImage;
     public List<Button> optionButtons;
 
     private List<Question> questions;
@@ -23,7 +24,8 @@ public class responder : MonoBehaviour
     private void LoadQuestionsFromJSON()
     {
         // Caminho do arquivo JSON contendo as perguntas
-        string filePath = Path.Combine(Application.streamingAssetsPath, "novas_perguntas.json");
+        string directoryPath = Path.Combine(Application.dataPath, "DoctorQuiz");
+        string filePath = Path.Combine(directoryPath, "novas_perguntas.json");
 
         // Verifica se o arquivo existe
         if (File.Exists(filePath))
@@ -42,10 +44,6 @@ public class responder : MonoBehaviour
             // Exibe uma mensagem de erro caso o arquivo não seja encontrado
             Debug.LogError("Arquivo JSON de perguntas não encontrado: " + filePath);
         }
-        
-        // Embaralha as perguntas e exibe a primeira pergunta
-        ShuffleQuestions();
-        DisplayQuestion();
     }
 
     private void ShuffleQuestions()
@@ -53,42 +51,50 @@ public class responder : MonoBehaviour
         // Embaralhar a lista de perguntas
         questions = questions.OrderBy(q => Random.Range(0, 1000)).ToList();
 
-        // Embaralhar as opções de cada pergunta
+        // Embaralhar as opções de cada pergunta e manter a alternativa correta na posição correta
         foreach (Question question in questions)
         {
-            question.options = question.options.OrderBy(o => Random.Range(0, 1000)).ToList();
+            // Obtenha a alternativa correta antes do embaralhamento
+            string correctOption = question.options[question.correctOptionIndex];
+
+            // Embaralhar as opções de resposta, excluindo a alternativa correta
+            List<string> shuffledOptions = question.options.Where(o => o != correctOption).ToList();
+            shuffledOptions = shuffledOptions.OrderBy(o => Random.Range(0, 1000)).ToList();
+
+            // Insira a alternativa correta em uma posição aleatória
+            int correctOptionPosition = Random.Range(0, shuffledOptions.Count + 1);
+            shuffledOptions.Insert(correctOptionPosition, correctOption);
+
+            // Atualize a lista de opções embaralhadas na pergunta
+            question.options = shuffledOptions;
         }
     }
-
 
     // Exibe a pergunta atual na interface do usuário
     private void DisplayQuestion()
     {
         // Verifica se ainda existem perguntas na lista
         if (questions.Count > 0)
-        {   
-            // Check if the question has an image
-            string imageCaminho = "Assets/Images" + currentQuestion.imagePath;
-            questionImage.sprite = Resources.Load<Sprite>(imagePath);
-            if (!string.IsNullOrEmpty(currentQuestion.imagePath))
-            {
-                // Enable image display
-                questionImage.gameObject.SetActive(true);
-                // Load the image using the imagePath (can be a URL, local path, etc.)
-                // questionImage.sprite = LoadImage(currentQuestion.imagePath);
-            }
-            else
-            {
-                // Disable image display
-                questionImage.gameObject.SetActive(false);
-            }
-
+        {
             // Obtém a primeira pergunta da lista
             currentQuestion = questions[0];
             questionText.text = currentQuestion.questionText;
 
-            // Atualiza o texto da pergunta na interface do usuário
-            questionText.text = currentQuestion.questionText;
+            // Verifica se a pergunta possui uma imagem
+            if (!string.IsNullOrEmpty(currentQuestion.questionImage))
+            {
+                // Ativa a exibição da imagem
+                questionImage.gameObject.SetActive(true);
+
+                // Carrega a imagem usando o caminho da imagem (pode ser um URL, caminho local, etc.)
+                string imagePath = Path.Combine(Application.dataPath, "DoctorQuiz", "Assets", "Images", currentQuestion.questionImage);
+                LoadImage(imagePath);
+            }
+            else
+            {
+                // Desativa a exibição da imagem
+                questionImage.gameObject.SetActive(false);
+            }
 
             // Atualiza o texto das opções nos botões correspondentes
             for (int i = 0; i < optionButtons.Count; i++)
@@ -98,6 +104,15 @@ public class responder : MonoBehaviour
         }
     }
 
+    // Carrega uma imagem
+    private void LoadImage(string imagePath)
+    {
+        // Carrega a textura da imagem
+        Texture2D texture = Resources.Load<Texture2D>(imagePath);
+
+        // Cria um sprite com base na textura e atribui ao componente de imagem
+        questionImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+    }
 
     // Verifica a resposta selecionada pelo jogador
     public void CheckAnswer(int optionIndex)
@@ -131,6 +146,7 @@ public class responder : MonoBehaviour
     public class Question
     {
         public string questionText;                // Texto da pergunta
+        public string questionImage;               // Caminho da imagem da pergunta
         public List<string> options;               // Lista de opções de resposta
         public int correctOptionIndex;             // Índice da opção correta na lista de opções
     }
@@ -141,10 +157,3 @@ public class responder : MonoBehaviour
         public List<Question> questions;           // Lista de perguntas
     }
 }
-
-// Lógica do código:
-// - O método Start() é chamado quando o objeto é inicializado, ele carrega as perguntas do arquivo JSON, embaralha as perguntas e exibe a primeira pergunta.
-// - LoadQuestionsFromJSON(): Carrega as perguntas do arquivo JSON e as atribui à lista de perguntas "questions".
-// - ShuffleQuestions(): Embaralha as perguntas e as opções de resposta usando valores aleatórios.
-// - DisplayQuestion(): Exibe a pergunta atual na interface do usuário, atualizando o texto da pergunta e das opções de resposta nos botões.
-// - CheckAnswer(int optionIndex): Verifica se a resposta selecionada pelo jogador está correta, exibindo uma mensagem correspondente. Remove a pergunta atual da lista e exibe a próxima pergunta, se houver, ou exibe uma mensagem de conclusão do quiz.
