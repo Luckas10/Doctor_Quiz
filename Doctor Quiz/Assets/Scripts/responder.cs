8a3b7cdefa91d76ceb2fab59226a0eda0d4795b5
Junior <69211250+juniorjse@users.noreply.github.com>
2023-09-27 21:18:55 -0300

    update

--- Doctor Quiz/Assets/Scripts/responder.cs
+++ Doctor Quiz/Assets/Scripts/responder.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using UnityEngine.SceneManagement;
 using Mono.Data.Sqlite;
-using Mono.Data;
 using System.Data;
 
-public class responder : MonoBehaviour
+public class Responder : MonoBehaviour
 {
     public Text questionText;
     public Image questionImage;
@@ -20,68 +17,88 @@ public class responder : MonoBehaviour
 
     private static float valueProgressLevel = 0.0f;
     private List<Question> questions;
-    private Question currentQuestion;
-    public static int correctQuestions;
+    private int currentQuestionIndex = 0;
 
-    private string path;
+    private string pathToDB;
     private string imageDirectoryPath;
-    private static int questionsAnswered = 0;
-    private List<Question> questionsShown = new List<Question>();
+    private int questionsAnswered = 0;
+    private int correctQuestions = 0; // Variável para contar as respostas corretas
 
     void Start()
     {
-        imageDirectoryPath = Path.Combine(Application.dataPath);
-        path = Path.Combine(Application.dataPath, "Scripts", "DoctorQuiz", "novas_perguntas.json");
-        LoadQuestions();
-        SetQuestion();
-        Debug.Log("questões respondidas Start: "+ questionsAnswered);
-    }
+        imageDirectoryPath = Application.dataPath;
+        pathToDB = Application.dataPath + "/" + DataBaseName;
 
+        LoadQuestionsForUser(1); // Carregue as questões para o usuário com ID 1
 
-    void LoadQuestions()
-    {
-        string json = File.ReadAllText(path);
-        QuestionList questionList = JsonUtility.FromJson<QuestionList>(json);
-        questions = questionList.questions.ToList();
-        foreach (Question question in questions)
-    {
-        //Debug.Log("Question: " + question.questionText);
-        //Debug.Log("Image: " + question.questionImage);
-        //Debug.Log("Options: " + string.Join(",", question.options));
-        //Debug.Log("Correct Option Index: " + question.correctOptionIndex);
-    }
-
-    }
-
-    private void LoadImage(string imageName)
-    {           string imagePath = Path.Combine(imageDirectoryPath, imageName);
-        if (File.Exists(imagePath))
-        {               byte[] imageData = File.ReadAllBytes(imagePath);
-            Texture2D texture = new Texture2D(2, 2);
-            texture.LoadImage(imageData);
-            questionImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        if (questions.Count > 0)
+        {
+            SetQuestion(currentQuestionIndex);
         }
         else
         {
-            Debug.LogError("Arquivo da imagem não encontrado: " + imagePath);
+            Debug.Log("Não há questões disponíveis para este usuário.");
         }
     }
 
-
-    void SetQuestion()
+    void LoadQuestionsForUser(int userId)
     {
-        txtProgress.text = (questionsAnswered + 1).ToString() + " / " + questions.Count.ToString();
-        progressLevel.value = valueProgressLevel;
-        if (questionsShown.Count < questions.Count)
-        {
-            // Seleciona uma questão aleatória que ainda não foi mostrada
-            List<Question> remainingQuestions = questions.Except(questionsShown).ToList();
-            int randomIndex = Random.Range(0, remainingQuestions.Count);
-            currentQuestion = remainingQuestions[randomIndex];
-            questionsShown.Add(currentQuestion);
+        questions = new List<Question>();
 
-            // Exibe a pergunta e a imagem, se houver
+        using (SqliteConnection dbConnection = new SqliteConnection("URI=file:" + pathToDB))
+        {
+            dbConnection.Open();
+
+            using (SqliteCommand dbCmd = dbConnection.CreateCommand())
+            {
+                dbCmd.CommandText = "SELECT q.*, qu.id_questao as responded " +
+                                    "FROM questoes q " +
+                                    "LEFT JOIN questao_usuario qu ON q.id = qu.id_questao AND qu.id_usuario = @UserId";
+
+                dbCmd.Parameters.Add(new SqliteParameter("@UserId", userId));
+
+                using (SqliteDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Question question = new Question
+                        {
+                            questionText = reader["enunciado"].ToString(),
+                            questionImage = reader["caminho_imagem"].ToString(), // Obtém o caminho da imagem da tabela questoes
+                            alternativa_a = reader["alternativa_a"].ToString(),
+                            alternativa_b = reader["alternativa_b"].ToString(),
+                            alternativa_c = reader["alternativa_c"].ToString(),
+                            alternativa_d = reader["alternativa_d"].ToString(),
+                            opcao_correta = reader["opcao_correta"].ToString()
+                        };
+                        questions.Add(question);
+
+                        // Verifique se a questão já foi respondida pelo usuário
+                        int responded = reader["responded"] != DBNull.Value ? reader.GetInt32(reader.GetOrdinal("responded")) : 0;
+                        if (responded > 0)
+                        {
+                            questionsAnswered++;
+                        }
+                    }
+                }
+            }
+
+            dbConnection.Close();
+        }
+    }
+
+    void SetQuestion(int questionIndex)
+    {
+        if (questionIndex >= 0 && questionIndex < questions.Count)
+        {
+            Question currentQuestion = questions[questionIndex];
+
+            txtProgress.text = (questionsAnswered + 1).ToString() + " / " + questions.Count.ToString();
+            progressLevel.value = valueProgressLevel;
+
+            // Exiba a pergunta e imagem
             questionText.text = currentQuestion.questionText;
+
             if (!string.IsNullOrEmpty(currentQuestion.questionImage))
             {
                 LoadImage(currentQuestion.questionImage);
@@ -92,67 +109,80 @@ public class responder : MonoBehaviour
                 questionImage.gameObject.SetActive(false);
             }
 
-            // Embaralha as opções e define o texto dos botões de opção
-            List<string> options = currentQuestion.options.ToList();
-            options = options.OrderBy(option => Random.value).ToList();
-            int selectedOptionIndex = -1;
+            // Defina o texto dos botões de opção
+            optionButtons[0].GetComponentInChildren<Text>().text = currentQuestion.alternativa_a;
+            optionButtons[1].GetComponentInChildren<Text>().text = currentQuestion.alternativa_b;
+            optionButtons[2].GetComponentInChildren<Text>().text = currentQuestion.alternativa_c;
+            optionButtons[3].GetComponentInChildren<Text>().text = currentQuestion.alternativa_d;
 
-            for (int i = 0; i < optionButtons.Count; i++)
-            {
-                int optionIndex = i;
-                optionButtons[optionIndex].GetComponentInChildren<Text>().text = options[optionIndex];
-
-                // Remove todos os listeners do botão de opção
-                optionButtons[optionIndex].onClick.RemoveAllListeners();
-
-                // Adiciona um listener que armazena o índice da opção selecionada na variável selectedOptionIndex
-                optionButtons[optionIndex].onClick.AddListener(() =>
-                {
-                    selectedOptionIndex = optionIndex;
-                    confirmButton.interactable = true;
-                });
-            }
-
-
-            // Remove todos os listeners do botão de confirmação
+            // Remova todos os ouvintes do botão de confirmação
             confirmButton.onClick.RemoveAllListeners();
 
-            // Adiciona um único listener que chama a função ConfirmAnswer() com o índice da opção selecionada
+            // Adicione um único ouvinte que chama a função ConfirmAnswer() com o índice da opção selecionada
             confirmButton.onClick.AddListener(() =>
             {
-                ConfirmAnswer(selectedOptionIndex, options[selectedOptionIndex]);
+                ConfirmAnswer(currentQuestionIndex);
             });
 
-            // Desabilita o botão de confirmação até que uma opção seja selecionada
+            // Desabilite o botão de confirmação até que uma opção seja selecionada
             confirmButton.interactable = false;
+        }
+        else
+        {
+            Debug.Log("Todas as questões foram respondidas.");
+            // Trate o caso em que todas as questões já foram respondidas.
         }
     }
 
-
-
-    void ConfirmAnswer(int index, string textoSelected)
+    private void LoadImage(string imageName)
     {
-        if (index != -1){
-            if (textoSelected == currentQuestion.correctOptionIndex)
+        string imagePath = Path.Combine(imageDirectoryPath, imageName);
+
+        if (File.Exists(imagePath))
+        {
+            byte[] imageData = File.ReadAllBytes(imagePath);
+            Texture2D texture = new Texture2D(2, 2);
+            texture.LoadImage(imageData);
+            questionImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+        else
+        {
+            Debug.LogError("Arquivo da imagem não encontrado: " + imagePath);
+        }
+    }
+
+    void ConfirmAnswer(int questionIndex)
+    {
+        if (questionIndex >= 0 && questionIndex < questions.Count)
+        {
+            Question currentQuestion = questions[questionIndex];
+
+            if (optionButtons[0].GetComponentInChildren<Text>().text == currentQuestion.opcao_correta)
             {
                 Debug.Log("Resposta correta!");
-                correctQuestions++;
+                correctQuestions++; // Incrementa a contagem de respostas corretas
             }
             else
             {
                 Debug.Log("Resposta incorreta!");
+                // Lógica para tratar a resposta incorreta
             }
+
             questionsAnswered++;
-            valueProgressLevel += 1.0f / questions.Count;
-            Debug.Log("questões respondidas ConfirmAnswer: "+ questionsAnswered);
-            if (questionsAnswered == questions.Count)
+            valueProgressLevel = (float)questionsAnswered / questions.Count;
+            Debug.Log("questões respondidas ConfirmAnswer: " + questionsAnswered);
+
+            currentQuestionIndex++;
+
+            if (currentQuestionIndex < questions.Count)
             {
-                SceneManager.LoadScene("Results");
-                Debug.Log("Respostas corretas: " + correctQuestions);
+                SetQuestion(currentQuestionIndex);
             }
             else
             {
-                SetQuestion();
+                Debug.Log("Todas as questões foram respondidas.");
+                Debug.Log("Respostas corretas: " + correctQuestions); // Exibe o número de respostas corretas
+                // Trate o caso em que todas as questões já foram respondidas.
             }
         }
     }
@@ -162,13 +192,10 @@ public class responder : MonoBehaviour
     {
         public string questionText;
         public string questionImage;
-        public string[] options;
-        public string correctOptionIndex;
-    }
-
-    [System.Serializable]
-    public class QuestionList
-    {
-        public Question[] questions;
+        public string alternativa_a;
+        public string alternativa_b;
+        public string alternativa_c;
+        public string alternativa_d;
+        public string opcao_correta;
     }
 }
