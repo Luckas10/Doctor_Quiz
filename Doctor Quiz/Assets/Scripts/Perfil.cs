f35ac28b1fe36dc729943b2d242294bb90811d44
Luckas10 <johndescraft@gmail.com>
2024-01-16 00:44:23 -0300

    add zerar quiz
    Função de zerar os questionários adicionada na cena de perfil.

--- Doctor Quiz/Assets/Scripts/Perfil.cs
+++ Doctor Quiz/Assets/Scripts/Perfil.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Mono.Data.Sqlite;
 using System.Data;
+using System;
+using System.IO;
+using UnityEngine.Networking;
 
 public class Perfil : MonoBehaviour
 {
@@ -12,9 +15,14 @@ public class Perfil : MonoBehaviour
     public Text textName;
     public string DataBaseName;
 
+    private string pathToDB;
+
     void Start()
     {
-        string conn = SetDataBaseClass.SetDataBase(DataBaseName + ".db");
+
+        ConnectionDB();
+
+        string conn = SetDataBaseClass.SetDataBase(DataBaseName);
         IDbConnection dbcon;
         IDbCommand dbcmd;
         IDataReader reader;
@@ -45,4 +53,62 @@ public class Perfil : MonoBehaviour
         dbcon = null;
 
     }
+
+    void ConnectionDB()
+    {
+        if (Application.platform != RuntimePlatform.Android)
+        {
+            pathToDB = Application.dataPath + "/StreamingAssets/" + DataBaseName;
+        }
+        else
+        {
+            pathToDB = Application.persistentDataPath + "/" + DataBaseName;
+
+            if (!File.Exists(pathToDB))
+            {
+                WWW load = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DataBaseName);
+                while (!load.isDone) { }
+
+                File.WriteAllBytes(pathToDB, load.bytes);
+            }
+        }
+    }
+
+    public void ZerarQuiz()
+    {
+        int id_usuario = PlayerPrefs.GetInt("id_usuario", -1);
+
+        if (id_usuario != -1)
+        {
+            using (SqliteConnection dbConnection = new SqliteConnection("URI=file:" + pathToDB))
+            {
+                dbConnection.Open();
+
+                using (SqliteCommand dbCmd = dbConnection.CreateCommand())
+                {
+                    dbCmd.CommandText = "DELETE FROM questao_usuario WHERE id_usuario = @UserId";
+                    dbCmd.Parameters.Add(new SqliteParameter("@UserId", id_usuario));
+
+                    int rowsAffected = dbCmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Debug.Log("Exclusão na tabela questao_usuario bem-sucedida. Todas as colunas para o usuário foram removidas.");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Nenhuma coluna foi encontrada para o usuário na tabela questao_usuario.");
+                    }
+                }
+
+                dbConnection.Close();
+            }
+        }
+        else
+        {
+            Debug.LogError("Erro ao obter o valor de id_usuario de PlayerPrefs.");
+        }
+    }
+
+
 }
